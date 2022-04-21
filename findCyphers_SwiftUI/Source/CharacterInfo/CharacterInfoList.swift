9498103e31c59a771f5c51ccb7be58d51f9e614f
LeeTaek <retake_joy@naver.com>
2022-04-21 11:56:12 +0900

    즐겨찾기 quick 구현

--- findCyphers_SwiftUI/Source/CharacterInfo/CharacterInfoList.swift
+++ findCyphers_SwiftUI/Source/CharacterInfo/CharacterInfoList.swift
@@ -9,25 +9,58 @@ import SwiftUI
 
 struct CharacterInfoList: View {
     
-    @EnvironmentObject var charcter : CharacterViewModel
-    
+    @EnvironmentObject var characterVM : CharacterViewModel
+    @State private var showingFavoriteCharacters: Bool = true
+
     var body: some View {
+        
         NavigationView {
-            List(charcter.characters) { cha in
-                HStack {
-                    CharacterInfoRow(character: cha)
-                    NavigationLink(destination: DetailCharacterInfo()) {
-                        EmptyView()
-                    }.frame(width: 0)
+            VStack {
+                
+                if showFavorite {
+                    favoriteCharacters
                 }
-                .buttonStyle(PlainButtonStyle())   //네비게이션보다 좋아요 버튼에 우선권을 줌.
-                    .navigationTitle("캐릭터 정보")
-                    .font(.body)
-        }
+                
+                Color.primary
+                    .opacity(0.3)
+                    .frame(maxWidth: .infinity, maxHeight: 1)
+                
+                characterList
+                
+            }   .buttonStyle(PlainButtonStyle())   //네비게이션보다 좋아요 버튼에 우선권을 줌.
+                .navigationTitle("캐릭터 정보")
+                .font(.body)
        
         }
 
     }
+    
+    //MARK: - 선호 캐릭터 보여줄까?
+    var showFavorite: Bool {
+        !characterVM.characters.filter{ $0.isFavorite }.isEmpty
+    }
+    
+    //MARK: - 선호 캐릭터 창
+    var favoriteCharacters: some View {
+        FavoriteCharacterScrollView(showingImage: $showingFavoriteCharacters)
+            .padding(.top, 24)
+            .padding(.bottom, 8)
+    }
+    
+    var characterList: some View {
+        List(characterVM.characters) { cha in
+            HStack {
+                CharacterInfoRow(character: cha)
+                NavigationLink(destination: DetailCharacterInfo()) {
+                    EmptyView()
+                }.frame(width: 0)
+            }
+         
+        }
+        
+    }
+    
+    
 }
 
 struct CharacterInfoList_Previews: PreviewProvider {
---
+++ findCyphers_SwiftUI/Source/CharacterInfo/FavoriteCharacterScrollView.swift
@@ -0,0 +1,109 @@
+//
+//  FavoriteCharacterScrollView.swift
+//  findCyphers_SwiftUI
+//
+//  Created by 이택성 on 2022/04/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct FavoriteCharacterScrollView: View {
+    @EnvironmentObject private var characterVM: CharacterViewModel
+    @Binding var showingImage: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            title
+            
+            if showingImage {
+                characters
+            }
+            
+        }.padding()
+    }
+    
+    
+    var title: some View {
+        HStack(alignment: .top, spacing: 5) {
+            Text("즐겨찾는 캐릭터")
+                .font(.headline)
+                .fontWeight(.medium)
+            
+            Image(systemName: "arrowtriangle.up.square")
+                .imageScale(.medium)
+                .foregroundColor(.renatocolor)
+                .padding(4)
+                .rotationEffect(Angle(radians: showingImage ? .pi : 0))
+
+            Spacer()
+            
+        }.padding(.bottom, 4)
+            .onTapGesture {
+                self.showingImage.toggle()
+            }
+        
+    }
+    
+    
+    var characters: some View {
+        let favoriteCharacters = characterVM.characters.filter { $0.isFavorite }
+        
+        return ScrollView(.horizontal, showsIndicators: false) {
+            HStack (spacing: 0) {
+                ForEach(favoriteCharacters) { character in
+                    NavigationLink(destination: DetailCharacterInfo()) {
+                        self.eachCharacter(character)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    func eachCharacter(_ character: CharacterInfo) -> some View {
+        GeometryReader { geometry in
+            let ImageURL = URL(string: API.CHARACTER_IMAGE_URL + character.characterId.characterId)
+            
+            VStack {
+                KFImage(ImageURL)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(Circle())
+                    .frame(width: 70, height: 70)
+           
+                
+                Text("\(character.characterId.characterName)")
+                    .font(.body)
+                    .fontWeight(.medium)
+                    
+            }.scaleEffect(self.scaledValue(from: geometry))
+                .padding(.top, 1)
+        
+            
+        }.frame(width: 105, height: 105)
+    }
+    
+    
+    func scaledValue(from geometry: GeometryProxy) -> CGFloat {
+        let xOffset = geometry.frame(in: .global).minX - 16 // 글로벌 좌표 기준으로 상품의 x값을 구하고, 그 값에 적용된 여백 값만큼 빼주면 한 contentOffset 얻을 수 있음.
+        
+        let minSize: CGFloat = 0.8
+        let maxSize: CGFloat = 1.0
+        let delta: CGFloat = maxSize - minSize
+        
+        let size = minSize + delta * (1 - xOffset / UIScreen.main.bounds.width)
+        
+        return max(min(size, maxSize), minSize)
+        
+    }
+    
+}
+
+//struct FavoriteCharacterScrollView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FavoriteCharacterScrollView(showingImage: true )
+//
+//    }
+//}
